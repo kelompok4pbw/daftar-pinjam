2760faf8b2695b8e701e13161eece71c851416bb
kelompok4pbw <160614818+kelompok4pbw@users.noreply.github.com>
2024-02-25 18:10:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Initialize Materialize components
     M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), { coverTrigger: false});
-    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+    M.Sidenav.init(document.querySelectorAll('.sidenav'), {edge: 'right'});
 });
